ff69aa5f5310294083c5b7a79a5aa8e7b98f754a
dhowe <daniel@rednoise.org>
2018-12-19 19:18:41 +0800

    electron options

--- main.js
+++ main.js
@@ -3,9 +3,10 @@ const {app, BrowserWindow} = electron;
 
 function createWindow () {
 
-  win = new BrowserWindow({ width: 800, height: 600 })
-  win.loadFile('index.html')
-  win.webContents.openDevTools();
+  win = new BrowserWindow({ width: 697, height: 535 });
+  win.loadFile('index.html');
+  //win.setFullScreen(true);
+  //win.webContents.openDevTools();
 }
 
 app.on('ready', createWindow)
