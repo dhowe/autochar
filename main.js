20b243e390d8af27f0f73c6f4dbf642f83352c7a
dhowe <daniel@rednoise.org>
2018-12-19 19:21:01 +0800

    auto-full screen on linux

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ function createWindow () {
 
   win = new BrowserWindow({ width: 697, height: 535 });
   win.loadFile('index.html');
-  //win.setFullScreen(true);
+  if (process.platform !== 'darwin') {
+    win.setFullScreen(true);
+  }
   //win.webContents.openDevTools();
 }
 
