7eb2036b52fb9b7ab5a75c0eda6f89401cd996ee
dhowe <daniel@rednoise.org>
2018-12-23 04:14:20 +0800

    sizing

--- main.js
+++ main.js
@@ -3,10 +3,11 @@ const {app, BrowserWindow} = require('electron');
 function createWindow () {
 
   // add w:17 w:45 for electron chrome
-  win = new BrowserWindow({ width: 697, height: 535 });
-  //win.setFullScreen(process.platform !== 'darwin');
-  win.webContents.openDevTools();
-  win.loadFile('index.html');
+  //win = new BrowserWindow({ width: 697, height: 535 });
+  win = new BrowserWindow({ width: 1024, height: 768 });
+  win.setFullScreen(process.platform !== 'darwin');
+  //win.webContents.openDevTools();
+  win.loadFile('sizing.html');
 }
 
 
