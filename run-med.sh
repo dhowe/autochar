badf097566b7e82dd6d0e81970aace1085915af1
dhowe <daniel@rednoise.org>
2018-12-23 14:20:31 -0500

    fast16gb

--- run-med.sh
+++ run-med.sh
@@ -1,2 +1,3 @@
-node --max-old-space-size=8192 writeAllMeds.js 9 -f 2>&1 | tee log.txt
+#!/bin/bash
+node --max-old-space-size=16384 writeAllMeds.js 3 -f 2>&1 | tee log.txt
 #ctrl-z, bg, disown
--- writeAllMeds.js
+++ writeAllMeds.js
@@ -1,25 +1,17 @@
 if (typeof Path2D == 'undefined') Path2D = (class Path2DMock {});
 
+let lang = 'trad';
 let fs = require('fs');
 let util = require('./cutils');
 
 let args = process.argv.slice(2);
 let maxAllowedMed = args && Math.min(args[0],10) || 10;
 let writeFile = (args && args.length > 1 && args[1] == '-f');
-let edgeFile = 'edges-dist'+maxAllowedMed+'.csv';
-let words = JSON.parse(fs.readFileSync('words-trad.json', 'utf8'));
+let edgeFile = 'edges-dist-'+lang+maxAllowedMed+'.csv';
+let words = JSON.parse(fs.readFileSync('words-'+lang+'.json', 'utf8'));
 let keys = Object.keys(words);
 
-console.log('Loaded '+keys.length+' words (med<='+maxAllowedMed+')');
-// 
-// function minEditDist(l1, l2) {
-//   var cost = Math.max(0, memMED(l1, l2) - 1);
-//   if (cost > maxAllowedMed) return Number.MAX_SAFE_INTEGER;;
-//   let e1 = util.getWord(l1).toEditStr();
-//   let e2 = util.getWord(l2).toEditStr();
-//   //console.log('minEditDistance', e1, 'vs', e2, cost);
-//   return memMED(e1, e2) + cost;
-// }
+if (writeFile) console.log('Target file: '+edgeFile);
 
 let edgeData = 'source,target,med';
 for (var i = 0; i < keys.length; i++) {
@@ -27,8 +19,7 @@ for (var i = 0; i < keys.length; i++) {
     if (i == j) continue;
     var word1 = keys[i];
     var word2 = keys[j];
-    //var med = util.minEditDistance(word1, word2);
-    var med = util.minEditDist(word1, word2);
+    var med = util.minEditDistance(word1, word2);
     if (med <= maxAllowedMed) {
         var line = word1+','+word2+','+med;
         if (writeFile) {
